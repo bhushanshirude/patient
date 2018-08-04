f13d7590b1cfea325dc36e6ded4e7e79ac6b008c
Your Name <you@example.com>
2018-08-04 12:14:42 +0530

    last commit

--- src/pages/patient-profile-own/patient-profile-own.ts
+++ src/pages/patient-profile-own/patient-profile-own.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { SocialSharing } from '../../../node_modules/@ionic-native/social-sharing';
+
 
 
 @Component({
@@ -7,9 +9,13 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'patient-profile-own.html'
 })
 export class PatientProfileOwnPage {
+  message: string = null;
+  link: string = null;
+  subject: string = null;
+  file: string = null;
   @Input('progress') progress;
   myDate = '2018-06-05';
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,private socialSharing: SocialSharing) {
     this.myDate = new Date().toISOString();
 
     this.progress = 56;
@@ -19,8 +25,9 @@ export class PatientProfileOwnPage {
 
 
   } 
-  share(){
-    
+  sharing() {
+    this.socialSharing.share(this.message, this.file, this.subject, this.link).then(() => {
+    }).catch(() => { });
   }
   sort(){}
   swipe(event) {
